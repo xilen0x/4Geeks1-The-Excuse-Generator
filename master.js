c691c6593d35b8b55451c1d58bb32edd8560e5f5
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-04-14 09:02:19 -0400

    modificada funcion en master.js

--- master.js
+++ master.js
@@ -1,11 +1,13 @@
 let who = ['the dog', 'my granma', 'his turtle', 'my bird'];
-let what = ['eat','pissed','crushed','broked'];
-let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+let what = ['eat', 'pissed', 'crushed', 'broked'];
+let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-var posicion1 = Math.floor(Math.random() * who.length-1) + 1;
-var posicion2 = Math.floor(Math.random() * what.length-1) + 1;
-var posicion3 = Math.floor(Math.random() * when.length-1) + 1;
 
-var conca = who [posicion1] + " " + what[posicion2] + " " + when[posicion3];
+document.getElementById("btn1").onclick = function () {
+    var posicion1 = Math.floor(Math.random() * who.length - 1) + 1;
+    var posicion2 = Math.floor(Math.random() * what.length - 1) + 1;
+    var posicion3 = Math.floor(Math.random() * when.length - 1) + 1;
 
-document.getElementById("excuse").innerHTML = conca;
+    var conca = who[posicion1] + " " + what[posicion2] + " " + when[posicion3];
+    document.getElementById("excuse").innerHTML = conca;
+}
